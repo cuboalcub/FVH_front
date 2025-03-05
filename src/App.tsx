dfe4aacaa7f35061d02715f6c2fffa30947c12c1
cuboalcub <zs22019951@estudiantes.uv.mx>
2025-03-05 09:17:44 -0600

    init project front

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import {  Text, View } from 'react-native';
+import '../global.css';
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center w-full h-full bg-gray-100">
+      <Text className='text-black text-4xl'>Hello World</Text>
+    </View>
+  );
+}
